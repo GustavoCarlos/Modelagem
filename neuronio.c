c8365fdec5f84d1e6b3c6a885fc5f42e7e613d27
GustavoCarlos <gustavo_carlos@ymail.com>
2016-05-12 22:35:59 -0300

    Commit com a adicao de comentario no arquivo neuronio.c

--- neuronio.c
+++ neuronio.c
@@ -4,12 +4,13 @@
 int main(int argc, const char * argv[])
 {
 	float dy, dx, dt, x, y, I, p, E, a, B, S;
-	
 
 	scanf("%f", &dx);
 	dy = tanh(dx);
 
 	printf("%f", dy);
+
+/*TEstando o commit do github*/
 
 }
 
