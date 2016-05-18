624a80bc708aade94f055c2567882e98e4bdcb2c
GustavoCarlos <gustavo_carlos@ymail.com>
2016-05-17 22:39:23 -0300

    Adicao das primeiras equacoes para um neuronio

--- neuronio.c
+++ neuronio.c
@@ -3,14 +3,28 @@
 
 int main(int argc, const char * argv[])
 {
-	float dy, dx, dt, x, y, I, p, E, a, B, S;
+	float dy, dx, dt, x, y, I, p, epsilon, alpha, beta, S;
+	
+	I = -0.02;
+	alpha = 0.5;
+	epsilon = 0.02;
+	beta = 0.1;
+	dt = 0.01;
+	x = 0;
+	y = 0;
 
-	scanf("%f", &dx);
-	dy = tanh(dx);
+	float i;
 
-	printf("%f", dy);
+	for(i = 0; i < 0.1; i += dt)
+	{
+		dx = (3*x - x*x*x + 2 - y + I) * dt;
+		dy = (epsilon*(alpha*(1 + tanh(x/beta)) - y))*dt;
+		x += dx;
+		y += dy;
+		
+		printf("X: %f Y: %f\r\n", x, y);
+	}
 
-/*TEstando o commit do github*/
 
 }
 
